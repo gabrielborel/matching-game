29ef62f3bead878cfd8ace2c0dc1ae838aecb3f7
Gabriel Borel <biel_borel@hotmail.com>
2022-01-02 11:40:54 -0300

    First Commit

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import './style.css'
+
+function CardGame(icon = 'alura-pixel', alt = 'Logo da alura') {
+  return /*html*/`
+    <article class="card-game">
+      <img src="src/images/${icon}.png" alt="${alt}">
+    </article>
+  `
+}
+
+export default CardGame
---
+++ src/objects/BoardGame/index.js
@@ -0,0 +1,15 @@
+import './style.css'
+import CardFrontBack from '../../components/CardFrontBack'
+
+function BoardGame(amountCards) {
+  const $htmlCardFrontBack = CardFrontBack()
+  const $htmlContent = $htmlCardFrontBack.repeat(amountCards)
+
+  return /*html*/`
+    <section class="board-game">
+      ${$htmlContent}
+    </section>
+  `
+}
+
+export default BoardGame
