859beb014218ae2e49e24ebb10e3ec89922c1630
Gabriel Borel <biel_borel@hotmail.com>
2022-01-02 22:36:14 -0300

    :construction: ArrowDown component finished

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -2,10 +2,12 @@ import './style.css'
 import PlayerName from '../../components/PlayerName'
 import VsPlayer from '../../components/VsPlayer'
 import PlayerScore from '../../components/PlayerScore'
+import ArrowDown from '../../components/ArrowDown'
 
 function ScoreBoard() {
   return /*html*/`
     <header class="score-board">
+      ${ArrowDown(1)}
       ${PlayerName('Player1')}
       ${PlayerScore(2)}
       ${VsPlayer()}
